bfdd00a9b3e28ead228a0d6e60a045d77ac1e5ec
jjkst <karthi.jayaraman@gmail.com>
2026-05-10 10:11:26 -0400

    use right env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+import { LibEnvironment } from 'ruku-bookings';
+
+export const environment: LibEnvironment = {
+  production: true,
+  apiBaseUrl: 'https://jk-dev.site/api',
+  googleClientId: '',
+  githubClientId: '',
+  githubRedirectUri: 'https://bookings.jk-dev.site/login'
+};
