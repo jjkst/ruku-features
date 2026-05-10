f23676e1cb6fe24f844648766fb05c5bc94ca9c9
jjkst <karthi.jayaraman@gmail.com>
2026-05-10 10:54:02 -0400

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { LibEnvironment } from 'ruku-bookings';
 
 export const environment: LibEnvironment = {
   production: true,
-  apiBaseUrl: 'https://jk-dev.site/api',
+  apiBaseUrl: 'https://bookings.jk-dev.site/api',
   googleClientId: '',
   githubClientId: '',
   githubRedirectUri: 'https://bookings.jk-dev.site/login'
